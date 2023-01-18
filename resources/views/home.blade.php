4135426e0b202ecbc4be63aee82c7758bb5a1be0
bnitin6034 <bnitin19@tbc.edu.np>
2023-01-18 20:07:38 +0545

    User's dashboard modidfied

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -15,6 +15,76 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+<link rel="stylesheet" href="css/dashboard.css">
+<link rel="stylesheet" href="css/bookstyle.css">
+<div class="container bg-white" id="customcontainer">
+    <h5 class="d-flex justify-content-between p-2">
+        <div>
+            <span class="border-red ">
+                Your
+            </span>
+            {{ __('Dashboard') }}
+        </div>
+</div>
+<div class="page-content page-container" id="page-content">
+    <div class="padding d-flex justify-content-center">
+        <div class="row container d-flex justify-content-center">
+            <div class="col-xl-6 col-md-12">
+                <div class="card user-card-full">
+                    <div class="row m-l-0 m-r-0">
+                        <div class="col-sm-4 bg-c-lite-green user-profile">
+                            <div class="card-block text-center text-white">
+                                <div class="m-b-25">
+                                    <img src="images/user.png" class="img-radius"
+                                        alt="User-Profile-Image">
+                                </div>
+                                <h6 class="f-w-600">{{ Auth::user()->name }}</h6>
+                                <p>Welcome to Essential Mart </p>
+                                <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <div class="card-block">
+                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">General Information</h6>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Email</p>
+                                        <h6 class="text-muted f-w-400">{{ Auth::user()->email }}</h6>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Role</p>
+                                        <h6 class="text-muted f-w-400">{{ Auth::user()->role }}</h6>
+                                    </div>
+                                </div>
+                                <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Created at</p>
+                                        <h6 class="text-muted f-w-400">{{ Auth::user()->created_at }}</h6>
+                                    </div>
+                                </div>
+                                <ul class="social-link list-unstyled m-t-40 m-b-10">
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
+                                            data-original-title="facebook" data-abc="true"><i
+                                                class="mdi mdi-facebook feather icon-facebook facebook"
+                                                aria-hidden="true"></i></a></li>
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
+                                            data-original-title="twitter" data-abc="true"><i
+                                                class="mdi mdi-twitter feather icon-twitter twitter"
+                                                aria-hidden="true"></i></a></li>
+                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
+                                            data-original-title="instagram" data-abc="true"><i
+                                                class="mdi mdi-instagram feather icon-instagram instagram"
+                                                aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
